6a68b0bd83ea55559e5e4c0c89b49079a87b1fa9
Kostiantyn <konstanlebed8@gmail.com>
2025-12-16 13:42:51 +0100

    Add chat reducer to Redux store configuration

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counter/counterSlice";
 
+import chatReducer from "./chat/slices/chatSlice";
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     //місце для вашіх редюсерів :)
+
+    // Kostiantyn's chat reducer
+    chat: chatReducer,
   },
 });
