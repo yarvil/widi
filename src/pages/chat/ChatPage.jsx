429700eacfe1cd2fe2c4267b73684751080c7c89
Kostiantyn <konstanlebed8@gmail.com>
2026-02-08 23:50:15 +0100

    Add conversation loading logic to ChatPage component

--- src/pages/chat/ChatPage.jsx
+++ src/pages/chat/ChatPage.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadConversations } from "@/app/store/chat/chatThunks";
 
 import { AppContainer } from "./components/common/styles";
 import ChatAreaComponent from "./components/chat/ChatAreaComponents";
 import ConversationListComponent from "./components/sidebar/ConversationListComponent";
 
 const ChatPage = () => {
+  const dispatch = useDispatch();
+
+  const userId = useSelector((state) => state.chat.currentUser.id);
+
   const [isMobileChatOpen, setIsMobileChatOpen] = useState(true);
 
   const handleOpenChatlist = () => {
     setIsMobileChatOpen((prev) => !prev);
   };
+
+  useEffect(() => {
+    if (!userId) return;
+
+    dispatch(loadConversations(userId));
+  }, [userId, dispatch]);
 
   return (
     <AppContainer>
