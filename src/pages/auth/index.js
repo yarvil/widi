79c253ed9199b43bd24d79ca4851167f4ebc7c84
yaros <yaroslav8.b@gmail.com>
2026-02-11 20:55:52 +0200

    upd

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,6 +1,6 @@
 import AuthPage from "./authPage/AuthPage.jsx";
 import LoginPage from "./loginPage/LoginPage.jsx";
-import RegisterPage from "./registerPage/RegisterPage.jsx";
+import RegisterPage from "./RegisterPage/RegisterPage.jsx";
 import ForgotPasswordPage from "./forgotPasswordPage/ForgotPasswordPage.jsx";
 import ResetPasswordPage from "./resetPasswordPage/ResetPasswordPage.jsx";
 import VerificationPage from "./verificationPage/verificationPage.jsx";
