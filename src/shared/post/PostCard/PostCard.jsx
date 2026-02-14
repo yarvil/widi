08b5c0b1e812d50be2d6113756e21a5a1abc2d2b
yaros <yaroslav8.b@gmail.com>
2026-02-14 14:30:13 +0200

    wip

--- src/shared/post/PostCard/PostCard.jsx
+++ src/shared/post/PostCard/PostCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PropTypes from "prop-types";
 
