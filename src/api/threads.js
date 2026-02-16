2e1cb589768649d993fd61200cc63b6215704c15
Kostiantyn <konstanlebed8@gmail.com>
2026-02-16 17:50:44 +0100

    fix: remove leading slash from API request path in fetchThreads function

--- src/api/threads.js
+++ src/api/threads.js
@@ -1,7 +1,7 @@
 import { apiRequest } from "@/pages/auth/sendRequest";
 
 export async function fetchThreads() {
-  return await apiRequest("GET", "/api/chat/threads");
+  return await apiRequest("GET", "api/chat/threads");
 }
 
 export async function createThread(otherUserId) {
