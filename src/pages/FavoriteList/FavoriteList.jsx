a46f21bf394c39785b138459b9a256d1477a6431
Ruslan Teplenko <ruslanteplenko15@gmail.com>
2026-02-16 17:18:08 +0200

    updating saved

--- src/pages/FavoriteList/FavoriteList.jsx
+++ src/pages/FavoriteList/FavoriteList.jsx
@@ -1,13 +1,20 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import {
     savedPosts,
 } from "@/app/store/posts/postsSelectors";
 import PostCard from "@/shared/assets/components/post/PostCard/PostCard";
-import { FavoriteWrapper, Favorites } from './FavoriteListStyled'
+import {
+    FavoriteWrapper,
+    Favorites,
+    SaveWrapper,
+    TitleSave,
+    TextSave
+} from './FavoriteListStyled'
 import { selectorSearch } from "@/app/store/search/searchSelectors";
 import { fetchSavedPostsThunk } from "@/app/store/posts/postsSlice";
 import { useDispatch } from "react-redux";
+import PageHeader from "@/shared/ui/PageHeader/PageHeader";
 export default function FavoriteList() {
     const savePost = useSelector(savedPosts);
     const searchValue = useSelector(selectorSearch)
@@ -17,17 +24,28 @@ export default function FavoriteList() {
             post.text.toLowerCase().includes(searchValue.toLowerCase())
         )
         .sort((a, b) => b.createdTime - a.createdTime);
-     useEffect(() => {
-    dispatch(fetchSavedPostsThunk());
-  }, [dispatch]);
+    useEffect(() => {
+        dispatch(fetchSavedPostsThunk());
+    }, [dispatch]);
     return (
-        <Favorites>
-            <FavoriteWrapper>
-                {filteredPosts.map(post => (
-                   <PostCard key={post.postId} post={post} withTopLine = {false} withBottomLine = {false}  />
-                ))}
-            </FavoriteWrapper>
-        </Favorites>
+        <>
+
+            <Favorites>
+                <FavoriteWrapper noBorder={savePost.length === 0} >
+                    <PageHeader title="Bookmarks" />
+                    {savePost.length === 0 && (
+                        <SaveWrapper>
+                            <TitleSave>Save posts for later</TitleSave>
+                            <TextSave>Bookmark posts to easily find them again in the future</TextSave>
+                        </SaveWrapper>
+                    )}
+                    {filteredPosts.map(post => (
+                        <PostCard key={post.postId} post={post} withTopLine={false} withBottomLine={false} />
+                    ))}
+                </FavoriteWrapper>
+            </Favorites>
+
+        </>
 
 
 
