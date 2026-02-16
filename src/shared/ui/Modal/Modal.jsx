e319cf4edc67837a2e9aaa57ff1a3134427fbd3a
yaros <yaroslav8.b@gmail.com>
2026-02-16 23:02:07 +0200

    fixes

--- src/shared/ui/Modal/Modal.jsx
+++ src/shared/ui/Modal/Modal.jsx
@@ -4,6 +4,8 @@ import PropTypes from "prop-types";
 // import MainLogo from "@/shared/assets/logo/WiDi.svg?react";
 import { ModalWrapper, Modal, ModalContent, CloseButton } from "./Modal.styled";
 import { LogoType, LogoWrapper } from "../Header/HeaderStyled";
+import logo from "@/shared/assets/logo/logo-2.png";
+
 export default function ModalWindow(props) {
   const { isOpen, closeModal, children } = props;
   return (
@@ -18,7 +20,7 @@ export default function ModalWindow(props) {
               <LogoWrapper>
                 <LogoType>
                   <img
-                    src="src/shared/assets/logo/logo-2.png"
+                    src={logo}
                     alt=""
                     style={{
                       width: "100%",
